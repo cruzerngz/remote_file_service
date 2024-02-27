5720f812cd47e3bac54c3f5d880a628aba82b36e
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2024-02-27 22:00:24 +0800

    client stuff

---
+++ crates/rfs_client/src/args.rs
@@ -0,0 +1,16 @@
+//! Command-line args for client
+
+use std::{net::Ipv4Addr, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct ClientArgs {
+    /// The IPv4 address of the server.
+    #[clap(default_value_t = Ipv4Addr::LOCALHOST)]
+    pub target: Ipv4Addr,
+
+    /// The server port to connect to.
+    #[clap(default_value_t = rfs::defaults::DEFAULT_PORT)]
+    pub port: u16,
+}
--- crates/rfs_client/src/main.rs
+++ crates/rfs_client/src/main.rs
@@ -1,17 +1,24 @@
+mod args;
+
 use std::net::{Ipv4Addr, SocketAddrV4};
 
-use futures::AsyncWriteExt;
+use clap::Parser;
+use futures::{AsyncReadExt, AsyncWriteExt};
 // use rfs_core::middleware::ContextManager;
 use rfs::{interfaces::*, middleware::ContextManager};
+
+use crate::args::ClientArgs;
 
 #[tokio::main]
 async fn main() {
     std::env::set_var("RUST_LOG", "DEBUG");
     pretty_env_logger::init();
 
+    let args = ClientArgs::parse();
+
     let manager = ContextManager::new(
         Ipv4Addr::LOCALHOST,
-        SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3333),
+        SocketAddrV4::new(args.target, args.port),
     )
     .expect("failed to initialize context manager");
 
@@ -22,20 +29,28 @@ async fn main() {
     // let mut remote_file = rfs::fs::VirtFile::create(manager, "remote_file.txt")
     //     .await
     //     .expect("file creation error");
-
     // remote_file
-    //     .write("hello world asdlkmasldkmalskd\n".as_bytes())
+    // .write("hello world asdlkmasldkmalskd\n".as_bytes())
+    // .await
+    // .expect("failed to write to file");
+
+    // let contents = rfs::fs::read_to_string(manager, "remote_file.txt")
     //     .await
-    //     .expect("failed to write to file");
+    //     .expect("failed to read file");
+    // println!("contents: {}", contents);
 
-    // send malformed data
-    let payload = SimpleOpsSayHello::Request {
-        content: "what's up".to_string(),
-    };
+    PrimitiveFsOpsClient::write_append_bytes(
+        &manager,
+        "remote_file.txt".to_owned(),
+        "new line\n".as_bytes().to_vec(),
+    )
+    .await
+    .unwrap();
 
-    let resp = manager.invoke(payload).await;
+    // let req = PrimitiveFsOpsClient::Request {
+    //     path: "remote_file.txt".to_string(),
+    //     bytes: "new line".as_bytes().to_vec(),
+    // };
 
-    println!("response from remote: {:?}", resp);
-
-    println!("Hello, world!");
+    return;
 }
