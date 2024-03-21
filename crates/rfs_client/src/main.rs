6a66dc5861a15f2cec25a6d645f582448652dc8b
Jia Rui <61926974+cruzerngz@users.noreply.github.com>
2024-03-21 10:50:08 +0800

    move test stuff

--- crates/rfs_client/src/main.rs
+++ crates/rfs_client/src/main.rs
@@ -1,29 +1,24 @@
 mod args;
+mod test;
 mod ui;
 
 use std::{
     io::{self, Write},
     net::SocketAddrV4,
     sync::Arc,
-    time::Duration,
 };
 
 use args::ClientArgs;
 use clap::Parser;
 // use clap::Parser;
 use crossterm::event::{self, KeyCode, KeyEventKind};
-use futures::{AsyncRead, AsyncReadExt, AsyncWriteExt};
 use ratatui::{
     backend::CrosstermBackend,
     style::Stylize,
     widgets::{self, canvas::Context},
     Terminal,
 };
-use rfs::{
-    fs::VirtFile,
-    interfaces::{CallbackOpsClient, PrimitiveFsOpsClient, SimpleOpsClient},
-    middleware::*,
-};
+use rfs::middleware::*;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -102,7 +97,7 @@ async fn main() -> io::Result<()> {
 
     match args.test {
         true => {
-            test_mode(manager).await?;
+            test::test_mode(manager).await?;
             return Ok(());
         }
         false => (),
@@ -147,61 +142,4 @@ async fn render_loop<W: Write>(mut term: Terminal<CrosstermBackend<W>>) -> io::R
             }
         }
     }
-}
-
-/// Test various stuff out
-async fn test_mode(mut ctx: ContextManager) -> io::Result<()> {
-    log::info!("testing remote invocations");
-    let _ = SimpleOpsClient::say_hello(&mut ctx, "new configuration".to_string())
-        .await
-        .unwrap();
-
-    let mut file = VirtFile::open(ctx.clone(), "remote_file.txt").await?;
-
-    // let contents = rfs::fs::read_to_string(manager, "red_chips_v3.json").await?;
-
-    // println!("file contents: {}", contents);
-    // file.write_bytes(b"hello world").await?;
-    let data = file.read_bytes().await?;
-
-    println!("bytes read: {:?}", std::str::from_utf8(&data));
-
-    let cloned_ctx = ctx.clone();
-    // let cb_handle = tokio::spawn(async move {
-    //     CallbackOpsClient::register_file_update(&mut cloned_ctx, "remote_file.txt".to_owned())
-    //         .await
-    //         .expect("middleware should not fail")
-    // });
-
-    log::info!("spawning separate watch task");
-    let handle = tokio::spawn(async move {
-        log::debug!("watching file");
-        let mut file = VirtFile::open(cloned_ctx, "remote_file.txt").await.unwrap();
-
-        match file.watch().await {
-            Ok(c) => {
-                log::info!("successfully received file update");
-                c
-            }
-            Err(e) => {
-                log::error!("error watching file: {:?}", e);
-                return;
-            }
-        };
-    });
-
-    tokio::time::sleep(Duration::from_millis(200)).await;
-
-    log::info!("writing to file from another client");
-    let _ = file
-        .write_bytes(
-            "hello world hello world\n".as_bytes(),
-            rfs::interfaces::FileWriteMode::Insert(3),
-        )
-        .await?;
-    log::info!("wrote update to file");
-
-    handle.await.expect("thread join error");
-
-    Ok(())
 }
---
+++ crates/rfs_client/src/test.rs
@@ -0,0 +1,71 @@
+use std::io;
+use std::time::Duration;
+
+use rfs::fs::VirtFile;
+use rfs::interfaces::*;
+use rfs::middleware::ContextManager;
+
+/// Test various stuff out
+pub async fn test_mode(mut ctx: ContextManager) -> io::Result<()> {
+    log::info!("testing remote invocations");
+    let _ = SimpleOpsClient::say_hello(&mut ctx, "new configuration".to_string())
+        .await
+        .unwrap();
+
+    let mut file = VirtFile::open(ctx.clone(), "remote_file.txt").await?;
+
+    // let contents = rfs::fs::read_to_string(manager, "red_chips_v3.json").await?;
+
+    // println!("file contents: {}", contents);
+    // file.write_bytes(b"hello world").await?;
+    let data = file.read_bytes().await?;
+
+    println!("bytes read: {:?}", std::str::from_utf8(&data));
+
+    let cloned_ctx = ctx.clone();
+    // let cb_handle = tokio::spawn(async move {
+    //     CallbackOpsClient::register_file_update(&mut cloned_ctx, "remote_file.txt".to_owned())
+    //         .await
+    //         .expect("middleware should not fail")
+    // });
+
+    log::info!("spawning separate watch task");
+    let handle = tokio::spawn(async move {
+        log::debug!("watching file");
+        let mut file = VirtFile::open(cloned_ctx, "remote_file.txt").await.unwrap();
+
+        match file.watch().await {
+            Ok(c) => {
+                log::info!("successfully received file update");
+                c
+            }
+            Err(e) => {
+                log::error!("error watching file: {:?}", e);
+                return;
+            }
+        };
+    });
+
+    tokio::time::sleep(Duration::from_millis(200)).await;
+
+    log::info!("writing to file from another client");
+    let _ = file
+        .write_bytes(
+            "hello world hello world\n".as_bytes(),
+            rfs::interfaces::FileWriteMode::Insert(3),
+        )
+        .await?;
+    log::info!("wrote update to file");
+
+    handle.await.expect("thread join error");
+
+    log::info!("reading server's base directory");
+
+    let read_dir = rfs::fs::read_dir(ctx, ".").await?;
+
+    for entry in read_dir.iter() {
+        log::info!("{:?}", entry);
+    }
+
+    Ok(())
+}
